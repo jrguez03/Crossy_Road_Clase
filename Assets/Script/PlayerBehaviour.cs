afa9c166a64bb63da527e4d2e8ca1a66d1235990
jrguez03 <jokorguez@gmail.com>
2024-03-19 22:50:12 +0000

    Escenarios e algunas posibles soluciones con los colliders

--- Assets/Script/PlayerBehaviour.cs
+++ Assets/Script/PlayerBehaviour.cs
@@ -10,6 +10,10 @@ public class PlayerBehaviour : MonoBehaviour
     public float r_Duration = 0.25f;
     public GameObject r_Player;
 
+    bool p_CanMove = true;
+
+    Vector3 p_PreviousPosition;
+
     public void Awake()
     {
         r_Player = this.gameObject;
@@ -27,26 +31,50 @@ public class PlayerBehaviour : MonoBehaviour
 
     void MoveTarget(Vector3 m_Direction)
     {
-        if (m_Direction.x > 0)
+        if (p_CanMove == true)
         {
-            transform.eulerAngles = new Vector3(0, -90f, 0);
+            if (m_Direction.x > 0)
+            {
+                transform.eulerAngles = new Vector3(0, -90f, 0);
+            }
+            else if (m_Direction.x < 0)
+            {
+                transform.eulerAngles = new Vector3(0, 90f, 0);
+            }
+            else if (m_Direction.z > 0)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            else if (m_Direction.z < 0)
+            {
+                transform.eulerAngles = new Vector3(0, 180f, 0);
+            }
+
+            LeanTween.move(r_Player, r_Player.transform.position + new Vector3(m_Direction.normalized.x, 0, 0) / 2 + Vector3.up / 2, r_Duration / 2).setEase(LeanTweenType.easeOutQuad).setOnComplete(() =>
+            {
+                LeanTween.move(r_Player, r_Player.transform.position + new Vector3(m_Direction.normalized.x, 0, 0) / 2 - Vector3.up / 2, r_Duration / 2).setEase(LeanTweenType.easeOutQuad);
+            });
         }
-        else if (m_Direction.x < 0)
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
         {
-            transform.eulerAngles = new Vector3(0, 90f, 0);
-        }
-        else if (m_Direction.z > 0)
-        {
-            transform.eulerAngles = new Vector3(0, 180f, 0);
-        }
-        else if (m_Direction.z < 0)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
+            p_PreviousPosition = transform.position;
         }
 
-        LeanTween.move(r_Player, r_Player.transform.position + new Vector3(m_Direction.normalized.x, 0, 0) + Vector3.up, r_Duration / 2).setEase(LeanTweenType.easeOutQuad).setOnComplete(() =>
+        if (collision.gameObject.CompareTag("Obstacle"))
         {
-            LeanTween.move(r_Player, r_Player.transform.position + new Vector3(m_Direction.normalized.x, 0, 0) - Vector3.up, r_Duration / 2).setEase(LeanTweenType.easeOutQuad);
-        });
+            Debug.Log("choca");
+            //transform.position = p_PreviousPosition;
+            p_CanMove = false;
+            Invoke("CanMove", 1f);
+        }
+    }
+
+    private void CanMove()
+    {
+        p_CanMove = true;
     }
 }
